f23dbfec4c2ae9a444d9dddb37e6f2b0a1b3ffc3
loic <loicanaelbdf@gmail.com>
2023-01-02 14:31:21 -0800

    employees table completed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import reportWebVitals from './reportWebVitals'
 import Header from './components/header/header'
 import Home from './pages/home/home'
 import Footer from './components/footer/footer'
+import EmployeeList from './pages/employeeList/employeeList'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -13,6 +14,7 @@ root.render(
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/employees-list" element={<EmployeeList />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/pages/employeeList/employeeList.jsx
@@ -0,0 +1,200 @@
+import React, { useMemo, useState } from 'react'
+import './employeeList.css'
+import { NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faMagnifyingGlass,
+  faArrowUp,
+  faArrowDown,
+  faArrowRight,
+  faArrowLeft,
+} from '@fortawesome/free-solid-svg-icons'
+import COLUMNS from './columns'
+import {
+  useTable,
+  useSortBy,
+  usePagination,
+  useFilters,
+  useGlobalFilter,
+  useAsyncDebounce,
+} from 'react-table'
+import employeesMocked from '../../data/dataEmployeeMock'
+
+function GlobalFilter({ globalFilter, setGlobalFilter }) {
+  const [value, setValue] = useState(globalFilter)
+  const onChange = useAsyncDebounce((value) => {
+    setGlobalFilter(value || undefined)
+  }, 200)
+
+  return (
+    <div>
+      <span>Search:{''}</span>
+      <input
+        value={value || ''}
+        onChange={(e) => {
+          setValue(e.target.value)
+          onChange(e.target.value)
+        }}
+        placeholder="Search..."
+        className="header-search__input"
+      />
+      <FontAwesomeIcon
+        icon={faMagnifyingGlass}
+        className="header-search__icone"
+      />
+    </div>
+  )
+}
+
+const EmployeeList = () => {
+  const columns = useMemo(() => COLUMNS, [])
+  const data = employeesMocked
+
+  const tableInstance = useTable(
+    {
+      columns: columns,
+      data: data,
+    },
+    useGlobalFilter,
+    useFilters,
+    useSortBy,
+    usePagination
+  )
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    setPageSize,
+    canPreviousPage,
+    state,
+    canNextPage,
+    nextPage,
+    previousPage,
+    setGlobalFilter,
+    state: { pageIndex, pageSize },
+  } = tableInstance
+
+  return (
+    <div className="table">
+      <h1 className="table-title">Current Employees</h1>
+      <NavLink to="/" className="table-link">
+        Return Home
+      </NavLink>
+      <div className="table-header">
+        <div className="table-header__entries">
+          <span>Show</span>
+          <select
+            value={pageSize}
+            className="table-header__select"
+            onChange={(e) => {
+              setPageSize(Number(e.target.value))
+            }}
+          >
+            {[10, 20, 25, 50, 100].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                {pageSize}
+              </option>
+            ))}
+          </select>
+          <span>entries</span>
+        </div>
+        <div className="table-header__search">
+          <GlobalFilter
+            globalFilter={state.globalFilter}
+            setGlobalFilter={setGlobalFilter}
+          />
+        </div>
+      </div>
+
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render('Header')}
+                  <span>
+                    {column.isSorted ? (
+                      column.isSortedDesc ? (
+                        <FontAwesomeIcon
+                          icon={faArrowDown}
+                          className="table-content__icon"
+                        />
+                      ) : (
+                        <FontAwesomeIcon
+                          icon={faArrowUp}
+                          className="table-content__icon"
+                        />
+                      )
+                    ) : (
+                      ''
+                    )}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <nav className="table-nav">
+        <span className="table-nav__text">
+          Showing<span>{Number(pageIndex + 1)}</span>to
+          <span>{rows.length}</span>
+          of
+          <span>{rows.length}</span>
+        </span>
+
+        <ul className="table-nav__list">
+          <li>
+            <button
+              className="table-nav__list--button"
+              onClick={() => previousPage()}
+              disabled={!canPreviousPage}
+            >
+              {
+                <FontAwesomeIcon
+                  icon={faArrowLeft}
+                  className="table-nav__list__icon--left"
+                />
+              }
+              <span>Previous</span>
+            </button>
+          </li>
+          <li>
+            <p className="table-nav__list--text">{pageIndex + 1}</p>
+          </li>
+          <li>
+            <button
+              className="table-nav__list--button"
+              onClick={() => nextPage()}
+              disabled={!canNextPage}
+            >
+              <span>Next</span>
+              <FontAwesomeIcon
+                icon={faArrowRight}
+                className="table-nav__list__icon--right"
+              />
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default EmployeeList
